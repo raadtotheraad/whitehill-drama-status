75a5ee528f078ed61c56858a629f24ba417eb8dd
Raad <149825178+raadtotheraad@users.noreply.github.com>
2026-03-27 15:42:57 +0100

    Export update from ReFramer

--- assets/script/bootstrap.a1faaee645c6f65307e76ea5c00866d8f188a109.js
+++ assets/script/bootstrap.a1faaee645c6f65307e76ea5c00866d8f188a109.js
@@ -5,7 +5,7 @@ function decode(component) {
 	return decodeURIComponent(component);
 }
 const hostInfo = {
-	version: "7113b64",
+	version: "1a239ab",
 	scopeCanvas: JSON.parse("true"),
 };
 const projectIdRegex = /^\/projects\/(?<prefix>(?:(?:[A-Za-z0-9]+-)*[A-Za-z0-9]+--)?)(?<id>[A-Za-z0-9]{20})(?<accessToken>(?:-[A-Za-z0-9]+)?)/;
